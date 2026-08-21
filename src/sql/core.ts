3a1d89c652b8bd454fc535df4850991b4c79e473
Philip Z <hello@teaql.io>
2026-08-21 17:27:33 +0800

    Synchronize checker fixes with mutation roots

--- src/sql/core.ts
+++ src/sql/core.ts
@@ -355,6 +355,11 @@ export abstract class AbstractSQLTeaQLClient implements TeaQLDataService {
         this.userContext.insertResource('fixTime', new Date());
         try {
           checker.checkAndFix(this.userContext, mutation, results);
+          if (mutation.ledgerKey) {
+            for (const [field, value] of Object.entries(mutation.payload || {})) {
+              this.userContext.entityRoot.set(mutation.ledgerKey, field, value);
+            }
+          }
           this.userContext.translateCheckResults(results);
           if (results.length) throw new CheckException(results);
         } finally {
